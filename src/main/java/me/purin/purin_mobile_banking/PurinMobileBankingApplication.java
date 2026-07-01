5c38456d20a06fb3af3f0bb42e9324c42a7d113e
chnth <chanitha.y@gmail.com>
2026-07-01 17:20:19 +0700

    add spring boot initializr template.

---
+++ src/main/java/me/purin/purin_mobile_banking/PurinMobileBankingApplication.java
@@ -0,0 +1,13 @@
+package me.purin.purin_mobile_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurinMobileBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurinMobileBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/purin/purin_mobile_banking/PurinMobileBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.purin.purin_mobile_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurinMobileBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
